a609bcaabd340bdb124c75d39727937efc852ab2
LineNicacio <line.nicacio@hotmail.com>
2024-01-07 19:57:25 -0300

    first commit

---
+++ src/main/java/com/fiap/postech/techchallenge/fastfoodordermanagement/FastfoodOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.postech.techchallenge.fastfoodordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodOrderManagementApplication.class, args);
+	}
+
+}
