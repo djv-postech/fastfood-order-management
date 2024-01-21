90218d3adf88889ae3da6ec2af41a4a240d1416d
LineNicacio <line.nicacio@hotmail.com>
2024-01-21 20:40:24 -0300

    feat: CucumberTest

---
+++ src/test/java/com/fiap/postech/techchallenge/fastfoodordermanagement/bdd/DefinicaoPassosPedido.java
@@ -0,0 +1,73 @@
+package com.fiap.postech.techchallenge.fastfoodordermanagement.bdd;
+
+import com.fiap.postech.techchallenge.fastfoodordermanagement.PedidoHelper;
+import com.fiap.postech.techchallenge.fastfoodordermanagement.application.api.pedido.records.DadosCadastroPedido;
+import com.fiap.postech.techchallenge.fastfoodordermanagement.application.api.pedido.records.DadosPedido;
+import com.fiap.postech.techchallenge.fastfoodordermanagement.application.api.pedido.records.DadosProduto;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class DefinicaoPassosPedido {
+
+    private Response response;
+    private final String BASE_URL = "http://localhost:8080";
+    private final String ENDPOINT_API_PEDIDO = BASE_URL + "/produto";
+
+    @Quando("cadastrar um novo pedido")
+    public DadosPedido pedido() {
+
+        DadosCadastroPedido dadosCadastroPedido = PedidoHelper.gerarDadosCadastroPedido();
+
+        response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(dadosCadastroPedido)
+                .when()
+                .post(ENDPOINT_API_PEDIDO);
+
+        return response.then().extract().as(DadosPedido.class);
+    }
+
+    @Entao("o pedido é criado e enviado com sucesso")
+    public void o_pedido_é_cadastrado_com_sucesso() {
+        response.then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Entao("deve ser apresentado")
+    public void deve_ser_apresentado() {
+        response.then()
+                .body(matchesJsonSchemaInClasspath("schemas/dados_pedido.schema.json"));
+    }
+
+
+    // outro
+
+    @Quando("receber um pedido com dados de cliente inválidos")
+    public DadosPedido receber_pedido_com_dados_de_cliente_invalido() {
+
+        DadosCadastroPedido dadosCadastroPedidoDadosInvalidos = PedidoHelper.gerarDadosCadastroPedidoComEmailClienteNulo();
+
+        response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(dadosCadastroPedidoDadosInvalidos)
+                .when()
+                .post(ENDPOINT_API_PEDIDO);
+
+        return response.then().extract().as(DadosPedido.class);
+    }
+
+    @Entao("uma mensagem de erro deve ser apresentada")
+    public void uma_mensagem_de_erro_deve_ser_apresentada() {
+        response.then()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body(matchesJsonSchemaInClasspath("schemas/erro.schema.json"));
+    }
+
+
+}
